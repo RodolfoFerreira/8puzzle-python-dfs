7cf0292d6df6d1724e62b6d6dc72ea100c13949c
Rodolfo Ferreira <rodolfo_250@hotmail.com>
2021-07-01 19:57:00 -0300

    Adição de código para verificação de solucionabilidade

--- puzzle.py
+++ puzzle.py
@@ -121,25 +121,80 @@ def expandPuzzle(puzzleState):
 def arreq_in_list(myarr, list_arrays):
     return next((True for elem in list_arrays if np.array_equal(elem, myarr)), False)
 
+def getInvCount(arr) :
+    inv_count = 0
+    for i in range(0, 2) :
+        for j in range(i + 1, 3) :
+           
+            # Value 0 is used for empty space
+            if (arr[j][i] > 0 and arr[j][i] > arr[i][j]) :
+                inv_count += 1
+    return inv_count
+     
+
+def isSolvable(puzzle) :
+    invCount = getInvCount(puzzle)
+   
+    return (invCount % 2 == 0)
+
+def verifReachable(initialArr, finalArr):
+    inv = 0
+    initial = list(initialArr.flatten())
+    final = list(finalArr.flatten())
+    for posInit in range(0, 9): #vetor inicial
+        if (initial[posInit] != 0):
+            posFinal = final.index(initial[posInit])
+            aux=[] #cria uma lista com todos os elementos que podem ocorrer antes do atual
+            for x in range(0, posFinal):
+                if (final[x] != 0):
+                    aux.append(final[x])
+            #print str(initial[posInit]) 
+            #print aux
+            for i in range(posInit, 9):
+                for j in range(0, len(aux)):
+                    if (initial[i] == aux[j]):
+                        #print "inversao"
+                        inv = inv + 1
+    #print inv		
+    if (inv % 2) == 0:	
+        return True
+    else:
+        return False
+
+def printMatrix(matrix):
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = ["|" + fmt.format(*row) + "|" for row in s]
+    print("\n".join(table))
+    
 i = 0
 fringe.append(initState)
 expandedStates.append(initState)
 
-while(len(fringe) > 0):
-    print("-"*5 + "ITERAÇÃO: %d" %i)
-    print("-"*5 + "FRINGE: %d" %len(fringe))
-    print("-"*5 + "EXPANDED: %d" %len(expandedStates))
-    print("-"*5 + "MOVIMENTOS: %d" %len(visited))
-    print("-"*20)
+if(verifReachable(initState, goalState)):
+    print("-"*5 + "A combinação é solucionável" + "-"*5)
+    printMatrix(initState)
+    print("-"*5 + "PARA" + "-"*5)
+    printMatrix(goalState)
 
-    state = deepcopy(fringe[0])
-    visited.append(state)
+    while(len(fringe) > 0):
+        print("-"*5 + "ITERAÇÃO: %d" %i)
+        print("-"*5 + "FRINGE: %d" %len(fringe))
+        print("-"*5 + "EXPANDED: %d" %len(expandedStates))
+        print("-"*5 + "MOVIMENTOS: %d" %len(visited))
+        print("-"*20)
 
-    if (np.array_equal(state, goalState)):
-        print("Estado desejado encontrado em %d tentativa(s)" % i)
-        break
-    else:
-        expandPuzzle(state)
-        fringe.pop(0)
+        state = deepcopy(fringe[0])
+        visited.append(state)
 
-    i=i+1
+        if (np.array_equal(state, goalState)):
+            print("Estado desejado encontrado em %d tentativa(s)" % i)
+            break
+        else:
+            expandPuzzle(state)
+            fringe.pop(0)
+
+        i=i+1
+else:
+    print("A combinação não é solucionável")
